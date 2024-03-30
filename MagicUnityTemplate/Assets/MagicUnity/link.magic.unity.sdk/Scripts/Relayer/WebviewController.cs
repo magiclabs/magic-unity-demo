7029bba41a7f10a0094259655f0a4343613592a2
Jacob Bullock <jacob.bullock@gmail.com>
2024-03-29 17:09:29 -0700

    clean up

--- MagicUnityTemplate/Assets/MagicUnity/link.magic.unity.sdk/Scripts/Relayer/WebviewController.cs
+++ MagicUnityTemplate/Assets/MagicUnity/link.magic.unity.sdk/Scripts/Relayer/WebviewController.cs
@@ -21,17 +21,7 @@ namespace link.magic.unity.sdk.Relayer
 {
     public class WebviewController
     {
-        // Switch variable depending on OS
-#if !UNITY_EDITOR_WIN || !UNITY_STANDALONE_WIN
-        private readonly WebViewObject _webViewObject;
 
-#else
-        private GameObject windows_webView;
-        private GameObject webviewContainer;
-        public BaseUwbClientManager clientmanager;
-        private WebBrowserClient webClient;
-        RectTransform uwbRectTransform;
-#endif
         private readonly Dictionary<int, Func<string, bool>> _messageHandlers = new();
 
 
@@ -40,7 +30,12 @@ namespace link.magic.unity.sdk.Relayer
         private bool _relayerReady;
 
 
+// Windows and Non-Windows have different Browser implementations
 #if !UNITY_EDITOR_WIN || !UNITY_STANDALONE_WIN
+        // NON-WINDOWS
+
+        private readonly WebViewObject _webViewObject;
+
         public WebviewController()
         {
             // instantiate webview 
@@ -61,10 +56,31 @@ namespace link.magic.unity.sdk.Relayer
                 }
             );
         }
+
+        private void ShowBrowser() {
+            _webViewObject.SetVisibility(true);
+        }
+
+        private void HideBrowser() {
+            _webViewObject.SetVisibility(false);
+        }
+
+        internal void Load(string url) {
+             _webViewObject.LoadURL(url);
+        }
+
+        private void ExecuteJavaScript(string js) {
+             _webViewObject.EvaluateJS(js);
+        }
 #else
+        // WINDOWS
+
+        private WebBrowserClient webClient;
+        private RectTransform uwbRectTransform;
+
         public WebviewController()
         {
-            webviewContainer = new GameObject("WebViewObjectContainer");
+            GameObject webviewContainer = new GameObject("WebViewObjectContainer");
 
             //Create canvas
             Canvas canvas = webviewContainer.AddComponent<Canvas>();
@@ -129,96 +145,43 @@ namespace link.magic.unity.sdk.Relayer
             webBrowser.browserClient.communicationLayer = comsLayer;
             webBrowser.inputHandler = inputHandler;
 
-            clientmanager = uwbGameObject.GetComponent<BaseUwbClientManager>();
+            // configure browser settings
+            BaseUwbClientManager clientmanager = uwbGameObject.GetComponent<BaseUwbClientManager>();
             webClient = clientmanager.browserClient;
             webClient.jsMethodManager.jsMethodsEnable = true;
             webClient.initialUrl = "https://box.magic.link";
             webClient.RegisterJsMethod<string>("_cb", _cb);
 
+            // hide the browser by default
             uwbRectTransform.localScale = Vector3.zero;
         }
 
+         private void ShowBrowser() {
+            // this is a solution to be able to show the browser from its hidden state
+            uwbRectTransform.localScale = Vector3.one;
+         }
 
-         // GameObject.FindWithTag("Windows_Browser").GetComponent<BaseUwbClientManager>();
-        public void WebviewController2()
-        {
-            // WebBrowserClient browserClient = clientManager.GetComponent<WebBrowserClient>();
-            // browserClient.javascript = true;
-            // browserClient.initialUrl = "https://google.com";
-
-            // ----
-            // windows_webView_container = new GameObject("WebViewObjectContainer");
-            // Canvas canvas = windows_webView_container.AddComponent<Canvas>();
-            // CanvasScaler canvasScaler = windows_webView_container.AddComponent<CanvasScaler>();
-
-            // windows_webView = new GameObject("WebViewObject");
-            // windows_webView.transform.parent = windows_webView_container.transform;
-
-            // WebBrowserUIBasic browser = windows_webView.AddComponent<WebBrowserUIBasic>();
-            // clientmanager = windows_webView.GetComponent<BaseUwbClientManager>();
-
-            // browser.inputHandler = ScriptableObject.CreateInstance<WebBrowserOldInputHandler>();
-            // webClient = clientmanager.browserClient;
-            // webClient.jsMethodManager.jsMethodsEnable = true;
-            // webClient.javascript = true;
-            // EngineConfiguration config = ScriptableObject.CreateInstance<EngineConfiguration>();
-            // config.engineAppName = "UnityWebBrowser.Engine.Cef";
-            // EnginePlatformFiles file = new Engine.EnginePlatformFiles();
-            // file.platform = Platform.Windows64;
-            // file.engineFileLocation = "Packages/dev.voltstro.unitywebbrowser.engine.cef.win.x64/Engine~/";
-            // config.engineFiles = new EnginePlatformFiles[] { file };
-            // webClient.engine = config;
-            // webClient.communicationLayer = ScriptableObject.CreateInstance<TCPCommunicationLayer>();
-            // webClient.RegisterJsMethod<string>("_cb", _cb);
-
-            // ----     
-            clientmanager = GameObject.FindWithTag("Windows_Browser").GetComponent<BaseUwbClientManager>();
-            webClient = clientmanager.browserClient;
-            webClient.javascript = true;
-            webClient.jsMethodManager.jsMethodsEnable = true;
-            webClient.RegisterJsMethod<string>("_cb", _cb);
-
-            Debug.Log(clientmanager);
-            
-            // windows_webView = new GameObject("Windows_Webview");
-            // windows_webView.AddComponent<Windows_Handler>();
-            // windows_webView.tag = "win_web";
-
-            Debug.Log("launching Webview");
-
-            //  DelayAction(5.0f); 
-            //StartCoroutine(DelayAction());
+         private void HideBrowser() {
+            // this is a solution to be able to hide the browser but keep it actively processing
+            uwbRectTransform.localScale = Vector3.zero;
         }
-#endif
 
-        internal void Load(string url)
-        {
-            Debug.Log("Load.url: " + url);
-#if !UNITY_EDITOR_WIN || !UNITY_STANDALONE_WIN
-            _webViewObject.LoadURL(url);
-#else
-            Debug.Log("browser.Load()" + url);
+        internal void Load(string url) {
             DelayLoadUrl(url);
-           // TestAsync(url); // url "http://google.com"
-             //Invoke("Test", 5.0f);
-#endif
         }
 
-        // void Test() {
-        //     Debug.Log("Test");
-        //     // _relayerLoaded = true;
-        //     _relayerReady = true;
-        //     _dequeue();
-        // }
-
-        async void DelayLoadUrl(string url) {
+        private async void DelayLoadUrl(string url) {
             await Task.Delay(1500);
             _relayerLoaded = true;
             webClient.LoadUrl(url);
-            // await Task.Delay(2000);
-            // Test();
         }
 
+        private void ExecuteJavaScript(string js) {
+            webClient.ExecuteJs(js);
+        }
+#endif
+
+        // Common Methods
 
         // callback js hooks
         private void _cb(string msg)
@@ -226,10 +189,8 @@ namespace link.magic.unity.sdk.Relayer
             Debug.Log($"MagicUnity Received Message from Relayer: {msg}");
             // Do SimRle Relayer JSON Deserialization just to fetch ids for handlers
 
-#if !UNITY_EDITOR_WIN || !UNITY_STANDALONE_WIN
             var res = JsonUtility.FromJson<RelayerResponse<object>>(msg);
             var msgType = res.msgType;
-
             var method = msgType.Split("-")[0];
 
             switch (method)
@@ -239,10 +200,10 @@ namespace link.magic.unity.sdk.Relayer
                     _dequeue();
                     break;
                 case nameof(InboundMessageType.MAGIC_SHOW_OVERLAY):
-                    _webViewObject.SetVisibility(true);
+                    ShowBrowser();
                     break;
                 case nameof(InboundMessageType.MAGIC_HIDE_OVERLAY):
-                    _webViewObject.SetVisibility(false);
+                    HideBrowser();
                     break;
                 case nameof(InboundMessageType.MAGIC_HANDLE_EVENT):
                     //Todo Unsupported for now
@@ -251,40 +212,6 @@ namespace link.magic.unity.sdk.Relayer
                     _handleResponse(msg, res);
                     break;
             }
-#else
-            Debug.Log("CALLBACK");
-            Debug.Log(msg);
-
-            var res = JsonUtility.FromJson<RelayerResponse<object>>(msg);
-            var msgType = res.msgType;
-
-            var method = msgType.Split("-")[0];
-
-            Debug.Log("method: " + method);
-            switch (method)
-            {
-                case nameof(InboundMessageType.MAGIC_OVERLAY_READY):
-                    _relayerReady = true;
-                    _dequeue();
-                    break;
-                case nameof(InboundMessageType.MAGIC_SHOW_OVERLAY):
-                    //_webViewObject.SetVisibility(true);
-                    Debug.Log("SHOW BROWSER");
-                    uwbRectTransform.localScale = Vector3.one;
-                    break;
-                case nameof(InboundMessageType.MAGIC_HIDE_OVERLAY):
-                    //_webViewObject.SetVisibility(false);
-                    uwbRectTransform.localScale = Vector3.zero;
-                    Debug.Log("HIDE BROWSER");
-                    break;
-                case nameof(InboundMessageType.MAGIC_HANDLE_EVENT):
-                    //Todo Unsupported for now
-                    break;
-                case nameof(InboundMessageType.MAGIC_HANDLE_RESPONSE):
-                    _handleResponse(msg, res);
-                    break;
-            }
-#endif
         }
 
 
@@ -293,7 +220,6 @@ namespace link.magic.unity.sdk.Relayer
         /// </summary>
         internal void Enqueue(string message, int id, Func<string, bool> callback)
         {
-            Debug.Log("Enqueue: " + message);
             _queue.Enqueue(message);
             _messageHandlers.Add(id, callback);
             _dequeue();
@@ -301,20 +227,13 @@ namespace link.magic.unity.sdk.Relayer
 
         private void _dequeue()
         {
-            Debug.Log("_dequeue " + (_relayerLoaded ? "true" : "false") + " " + (_relayerReady ? "true" : "false") + " " + _queue.Count.ToString());
+            Debug.Log("_dequeue _relayerLoadedL" + (_relayerLoaded ? "true" : "false") + ", _relayerReady:" + (_relayerReady ? "true" : "false") + ", count:" + _queue.Count.ToString());
             if (_queue.Count != 0 && _relayerReady && _relayerLoaded)
             {
                 var message = _queue.Dequeue();
 
                 Debug.Log($"MagicUnity Send Message to Relayer: {message}");
-#if !UNITY_EDITOR_WIN || !UNITY_STANDALONE_WIN
-                _webViewObject.EvaluateJS(
-                    $"window.dispatchEvent(new MessageEvent('message', {{ 'data': {message} }}));");
-#else
-                //windows_webView.GetComponent<Windows_Handler>().launch($"window.dispatchEvent(new MessageEvent('message', {{ 'data': {message} }}));");
-                Debug.Log("ExecuteJs: " + $"window.dispatchEvent(new MessageEvent('message', {{ 'data': {message} }}));");
-                webClient.ExecuteJs($"window.dispatchEvent(new MessageEvent('message', {{ 'data': {message} }}));");
-#endif
+                ExecuteJavaScript($"window.dispatchEvent(new MessageEvent('message', {{ 'data': {message} }}));");
                 _dequeue();
             }
         }
