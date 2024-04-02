8afa23a145530f796d2481aecc7ec0f339f1546d
Jacob Bullock <jacob.bullock@gmail.com>
2024-04-02 03:24:08 -0500

    fixing result text

--- MagicUnityTemplate/Assets/MagicUnity/link.magic.unity.sdk/Prefabs/Scripts/MagicUnityButton.cs
+++ MagicUnityTemplate/Assets/MagicUnity/link.magic.unity.sdk/Prefabs/Scripts/MagicUnityButton.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using link.magic.unity.sdk;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class MagicUnityButton : MonoBehaviour
 {
-    public Text result;
+    public Magic magic;
+    [SerializeField] public TextMeshProUGUI result;
 
     [SerializeField] internal string OTPEmail;
 
